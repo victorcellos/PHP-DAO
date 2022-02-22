8f79d0946fbac4d3eb15502f7fccbdec4471d6fb
Victor Vasconcellos <victor.cellos2@gmail.com>
2022-02-22 18:24:01 -0300

    Adicionado comando Delete

--- class/usuario.php
+++ class/usuario.php
@@ -153,6 +153,22 @@ $this->setData($results[0]); nesse comando nos criamos um metodo e usamos ele pa
         ));
 
     }
+    
+    public function delete(){
+
+        $sql = new Sql();
+
+        $sql->execQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
+
 
 public function __construct($login = "", $password = ""){
     $this->setDeslogin($login);
--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <?php
 
 require_once("config.php");
-// Carrega um usuario
+
+/*       Carrega um usuario     */
+
 //$root = new Usuario();
 //$root->loadById(2);
 //echo $root;
 
-//================
+//==========================================
 
-//carrega uma lista de usuarios
+/*     carrega uma lista de usuarios       */
+
 // $lista = Usuario::getList();
 // echo json_encode($lista);
 
-//====================
+//==========================================
 
-//Carrega uma lista de usuarios buscando pelo login
+/*    Carrega uma lista de usuarios buscando pelo login      */
+
 // $search = Usuario::search("jo");
 // echo json_encode($search);
 
-//====================
+//==========================================
 
-//carrega um usuario usando o login e a senha
+/*       carrega um usuario usando o login e a senha      */
+
 // $usuario = new Usuario();
 // $usuario->login("Carlos", "53512321");
 // echo $usuario;
 
-//====================
-//insert de um usuario novo
+//==========================================
+
+/*        insert de um usuario novo        */
 
 // $aluno = new Usuario();
 // $aluno->setDeslogin("aluno");
@@ -34,28 +40,38 @@ require_once("config.php");
 // $aluno->insert();
 // echo $aluno;
 
-//====================
-//insert de um usuario novo porem menor e com o insert dentro da arquivo usuarios
+//==========================================
+
+/*          insert de um usuario novo porem menor e com o insert dentro da arquivo usuarios        */
 
 // $aluno = new Usuario("beto", "1234");
 // $aluno->insert();
 // echo $aluno;
 
-//====================
+//==========================================
 
-//Comando usado para dar UPDATE 
+/*          Comando usado para dar UPDATE  \\ Alterar um usuario             */ 
 
-$usuario = new Usuario();
-$usuario->loadById(8);
-$usuario->update("professor", "ggwpz");
-echo $usuario;
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "ggwpz");
+//echo $usuario;
+
+//==========================================
+
+/*          Comando para deletar             */
+
+//$usuario = new Usuario();
+//$usuario->loadById(7);
+//$usuario->delete();
+///echo $usuario;
 
 
-//====================
+//==========================================
 
 // Mostra todas os usuarios criado
-//$sql = new Sql();
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-//echo json_encode($usuarios);
+$sql = new Sql();
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+echo json_encode($usuarios);
 
 ?>
